998272aff83295663a22c769e40d0859a6629cf8
Midubo <47749932+Midubo@users.noreply.github.com>
2021-02-17 08:14:18 +0200

    7. Custom Draggable Window

--- BlueTimer/Windows/Window_About.xaml.cs
+++ BlueTimer/Windows/Window_About.xaml.cs
@@ -1,6 +1,8 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Diagnostics;
 using System.Linq;
+using System.Reflection;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows;
@@ -19,9 +21,61 @@ namespace BlueTimer
     /// </summary>
     public partial class Window_About : Window
     {
+        #region VARIABLES AND ON LOAD
+
+        #region Variables
+
+        MainWindow owner;
+
+        private string mWebsite = "https://www.youtube.com/c/MishaDubok";
+
+        #endregion
+
+        #region On Load
+
         public Window_About()
         {
             InitializeComponent();
+
+            string assemblyName = Assembly.GetExecutingAssembly().GetName().Name;
+            image_Sub.ToolTip = assemblyName;
+
+            Version version = Assembly.GetExecutingAssembly().GetName().Version;
+            tblc_Subheader.Text = string.Format(" - Version {0}.{1}.{2}", version.Major, version.Minor, version.Build);
         }
+
+        private void Window_Loaded(object sender, RoutedEventArgs e)
+        {
+            owner = Owner as MainWindow;
+        }
+
+        #endregion
+
+        #endregion
+
+        private void DragWithHeader(object sender, MouseButtonEventArgs e)
+        {
+            if (e.ChangedButton == MouseButton.Left)
+                DragMove();
+        }
+
+        private void image_MouseDown(object sender, MouseButtonEventArgs e)
+        {
+            Process.Start(mWebsite);
+        }
+
+        private void btn_Close_Click(object sender, RoutedEventArgs e)
+        {
+            Close();
+        }
+
+        private void btn_Visit_Click(object sender, RoutedEventArgs e)
+        {
+            Process.Start(mWebsite);
+        }
+
+        
+
+        
     }
 }
